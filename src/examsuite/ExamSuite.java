4ff1d699b268c2765c8ddb0d78d00eda8e0eba17
Gaurav Atul <asperanza23@gmail.com>
2018-04-02 16:37:45 +0530

    Creates a core java based quiz application

---
+++ src/examsuite/ExamSuite.java
@@ -0,0 +1,15 @@
+package examsuite;
+public class ExamSuite 
+{
+
+    mainFrame frm = new mainFrame();
+    public ExamSuite() 
+    {
+        frm.setVisible(true);
+        
+    }
+    public static void main(String[] args) {
+        new ExamSuite();
+    }
+    
+}
